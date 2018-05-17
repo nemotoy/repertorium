48a93e78ec5fed25d573dfac218dda93abd7a66f
sky0621 <sky14work@gmail.com>
2018-05-17 11:35:41 +0900

    continue

--- README.md
+++ README.md
@@ -3,19 +3,19 @@ GitHubから指定オーナーのリポジトリを取得
 
 ★作成中★
 
-## function
+## ■function
 
 ・フィルタリング（リポジトリ名の正規表現等）
 
 ・チェックアウトブランチ指定
 
-## ref
+## ■ref
 
 https://developer.github.com/v3/
 
 https://developer.github.com/v3/repos/#list-user-repositories
 
-## curl
+## ■curl
 <pre>
 curl -i https://api.github.com/users/sky0621/repos?per_page=200 > /tmp/sky0621repos.txt
 cat /tmp/sky0621repos.txt | grep clone_url
@@ -36,4 +36,12 @@ cat /tmp/sky0621repos.txt | grep clone_url
     "clone_url": "https://github.com/sky0621/study-springboot-cli.git",
     "clone_url": "https://github.com/sky0621/study-springboot-maven.git",
     "clone_url": "https://github.com/sky0621/study-terraform.git",
+</pre>
+
+## ■MEMO
+
+### add command
+<pre>
+cobra add parent
+cobra add child -p parentCmd
 </pre>
--- cmd/sub__listup.go
+++ cmd/sub__listup.go
@@ -20,9 +20,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// filterCmd represents the filter command
-var filterCmd = &cobra.Command{
-	Use:   "filter",
+// listupCmd represents the listup command
+var listupCmd = &cobra.Command{
+	Use:   "listup",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -31,20 +31,22 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("filter called")
+		fmt.Println("listup called")
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(filterCmd)
+	getCmd.AddCommand(listupCmd)
+	// TODO if I wanna add other command(f.e. set) and use same listupCmd, add this.
+	// setCmd.AddCommand(listupCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// filterCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// listupCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// filterCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// listupCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
